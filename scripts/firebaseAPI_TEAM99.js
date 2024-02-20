b8f127c9a696dc885d9f0bc0da74eaee4b067fdd
Andre Hindarmara <hindarmara@gmail.com>
2024-02-20 10:51:57 -0800

    I have made changes to demo 07

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+      apiKey: "AIzaSyAQRwAVZE-853a4LJFWLcJA3pmrq_H4xkw",
+      authDomain: "fir-d6348.firebaseapp.com",
+      projectId: "fir-d6348",
+      storageBucket: "fir-d6348.appspot.com",
+      messagingSenderId: "102578748717",
+      appId: "1:102578748717:web:fad6719b64b2e4a40de7a4",
+      measurementId: "G-GQ24THGM32"
+    };
 
 //--------------------------------------------
 // initialize the Firebase app
